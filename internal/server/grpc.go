68e8a85be06b1e0b16b52b90a07fe8a2aeafb620
zhouyang <zhouyang2021@163.com>
2023-08-19 01:47:57 +0800

    user service的gRPC服务注册

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -1,12 +1,15 @@
 package server
 
 import (
+	"warehouse/api/git"
 	v1 "warehouse/api/helloworld/v1"
 	"warehouse/internal/conf"
 	"warehouse/internal/service"
+	"warehouse/middleware"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/selector"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 )
 
@@ -15,6 +18,7 @@ func NewGRPCServer(c *conf.Server, greeter *service.GreeterService, logger log.L
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
+			selector.Server(middleware.Auth()).Path("/api.git.User/Login").Build(),
 		),
 	}
 	if c.Grpc.Network != "" {
@@ -28,5 +32,6 @@ func NewGRPCServer(c *conf.Server, greeter *service.GreeterService, logger log.L
 	}
 	srv := grpc.NewServer(opts...)
 	v1.RegisterGreeterServer(srv, greeter)
+	git.RegisterUserServer(srv, service.NewUserService())
 	return srv
 }
